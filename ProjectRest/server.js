fb5c228b4567ea9a434cf8709651aa44f717f6a3
Shyamalboharupi <82198220+Shyamalboharupi@users.noreply.github.com>
2021-07-08 11:17:11 +0530

    Add files via upload

---
+++ ProjectRest/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.eve.PORT||9800;
+const http = require('http');
+
+app.use(express.static(path.join(__dirname,'build')));
+app.get('/',(req,res)=>{ 
+res.sendFile(path.join(__dirname,'build/index.html'))
+
+});
+
+app.set('port',port);
+const server = http.createServer(app);
+server.listen(port);
